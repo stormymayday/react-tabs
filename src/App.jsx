ab85e7778002c876b632ac8014df271ed977e328
stormymayday <stormymayday7@gmail.com>
2023-08-29 14:05:26 -0700

    Implemented fetchData function. Imported in App.jsx and fetched the data.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,34 @@
+import { useEffect, useState } from "react";
+import fetchData from "./utils/fetchData.js";
+
 const url = 'https://course-api.com/react-tabs-project';
 
 const App = () => {
+
+  const [jobs, setJobs] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+
+    fetchData(url, setIsLoading, setIsError, setJobs);
+
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className="jobs-center">
+        <div className="loading"></div>
+      </section>
+    );
+  }
+
+  if (isError) {
+    return (
+      <div>Error</div>
+    );
+  }
+
   return <h2>Tabs Starter</h2>;
 };
 export default App;
