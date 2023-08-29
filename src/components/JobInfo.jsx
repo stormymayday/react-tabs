569f356c40e00bf899c255c5fc3414908950a4e1
stormymayday <stormymayday7@gmail.com>
2023-08-29 14:26:27 -0700

    Implemented JobDuties.jsx

--- src/components/JobInfo.jsx
+++ src/components/JobInfo.jsx
@@ -1,3 +1,5 @@
+import JobDuties from "./JobDuties.jsx";
+
 const JobInfo = ({ jobs }) => {
 
     const { company, dates, duties, title } = jobs[0];
@@ -7,6 +9,7 @@ const JobInfo = ({ jobs }) => {
             <h3>{title}</h3>
             <span className="job-company">{company}</span>
             <p className="job-date">{dates}</p>
+            <JobDuties duties={duties} />
         </article>
     );
 }
