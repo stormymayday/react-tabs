fb16c0ae1c7572b3686f11939cd9084ab67b604a
stormymayday <stormymayday7@gmail.com>
2024-11-25 18:53:19 -0800

    Duties component

--- components/Duties.tsx
+++ components/Duties.tsx
@@ -1,10 +1,24 @@
+import { FaAngleDoubleRight } from "react-icons/fa";
+import { v4 as uuidv4 } from "uuid";
+
 interface DutiesProps {
     duties: string[];
 }
 
 function Duties({ duties }: DutiesProps) {
-    console.log(duties);
-
-    return <div>Duties</div>;
+    return (
+        <div>
+            {duties.length > 0 &&
+                duties.map((duty) => {
+                    const id = uuidv4();
+                    return (
+                        <div key={id} className="job-desc">
+                            <FaAngleDoubleRight className="job-icon" />
+                            <p>{duty}</p>
+                        </div>
+                    );
+                })}
+        </div>
+    );
 }
 export default Duties;
